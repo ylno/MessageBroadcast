11ca1655630d9b740a2c578ee426590db72c5339
mfrankl <mfrankl@seibert-media.net>
2017-06-24 15:20:31 +0200

    repair

--- src/test/java/info/frankl/ApplicationRunnerTest.java
+++ src/test/java/info/frankl/ApplicationRunnerTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.fail;
-
 public class ApplicationRunnerTest {
 
   private static final Logger logger = LoggerFactory.getLogger(ApplicationRunnerTest.class);
@@ -15,8 +13,4 @@ public class ApplicationRunnerTest {
     logger.debug("test log");
   }
 
-  @Test
-  public void testFail() throws Exception {
-    fail();
-  }
 }
