dff5730a5db18cb05df3ab7493574d4ba0aab38b
mfrankl <mfrankl@seibert-media.net>
2017-06-24 15:18:36 +0200

    test jenkins fail

--- src/test/java/info/frankl/ApplicationRunnerTest.java
+++ src/test/java/info/frankl/ApplicationRunnerTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.Assert.fail;
+
 public class ApplicationRunnerTest {
 
   private static final Logger logger = LoggerFactory.getLogger(ApplicationRunnerTest.class);
@@ -11,5 +13,10 @@ public class ApplicationRunnerTest {
   @Test
   public void testInit() throws Exception {
     logger.debug("test log");
+  }
+
+  @Test
+  public void testFail() throws Exception {
+    fail();
   }
 }
