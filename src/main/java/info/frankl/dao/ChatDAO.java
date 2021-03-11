2615ae54caf71f37a7405ff80815d34dcc1eea55
mfrankl <mfrankl@seibert-media.net>
2021-03-11 18:35:45 +0100

    version

--- src/main/java/info/frankl/dao/ChatDAO.java
+++ src/main/java/info/frankl/dao/ChatDAO.java
@@ -27,7 +27,7 @@ public class ChatDAO {
   public ChatDAO(final String redisHost) {
     //    jedisPool = new JedisPool(new JedisPoolConfig(), redisHost);
     jedis = new Jedis(redisHost);
-    logger.debug("jedis init", jedis.clusterInfo());
+//    logger.debug("jedis init", jedis.clusterInfo());
   }
 
   public User getUser(final Integer id) {
